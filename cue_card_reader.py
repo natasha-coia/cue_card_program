cf0c25fd9e9331774dee5476c91a5a4c66d8f473
Tasha <theblankrogue@gmail.com>
2020-04-14 20:30:58 +0100

    Improved cue card reader
    Got rid of the rand_selector function, and instead of having a list of already-used lines, I deleted them from the list of lines.

--- cue_card_reader.py
+++ cue_card_reader.py
@@ -1,32 +1,23 @@
 import random
 
-
-def rand_selector(num,thing):
-    return thing[random.randint(0,num)]
-
 def read_cue_card(line):
     front = ""
     x = 0
     is_front = True
     while is_front == True: #a.k.a before the separator "/" has appeared
-        is_front = separator(line[x])
         front += line[x]
         x += 1
+        is_front = separator(line[x])
     print("Front of cue card: " + front)
-    next = input("When ready to see back of cue card, press Enter: ")
+    next = input("When ready to see back of cue card, press Enter")
     back = ""
+    x += 1
     while x < len(line): #output the rest of the line
         back += line[x]
         x += 1
     print("Back of cue card is: " + back)
     next = input("When ready to see next cue card, press Enter")
     return
-
-def already_read(alreadyread,x):
-    if x in alreadyread:
-        return True
-    else:
-        return False
 
 def separator(char):
     if char == "/":
@@ -36,10 +27,8 @@ def separator(char):
 topic_name = input("Enter name (filename) of first set of cuecards: ")
 topic = open(topic_name,'r') #opens the file of cue cards relating to topic to read
 lines = topic.readlines()   #creates a list containing each line in the file
-length = len(lines) #finds the length (number of lines) of the file
-alreadyread=[]
-while len(alreadyread) < length:    #while there is still lines to be read
-    line = rand_selector(length,lines)  #randomly select a line
-    if not already_read(alreadyread,line):  #if it hasn't already been read, read the cue card!
-        read_cue_card(line)
-        alreadyread.append(line)    #then add line to list of already read lines
+while len(lines) > 0:    #while there is still lines to be read
+    line = lines[random.randint(0,len(lines))]  #randomly select a line
+    read_cue_card(line)
+    lines.remove(line)
+print("No more cue cards left on this topic")
