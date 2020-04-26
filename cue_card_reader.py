9277c66d0e7125c1d3d15182f28594c742344147
Tasha <theblankrogue@gmail.com>
2020-04-26 12:02:23 +0100

    Added menu and ability to exit
    Added the main menu function (not yet able to do options 2-4), as well as adding the option to exit the topic between reading cue cards.

--- cue_card_reader.py
+++ cue_card_reader.py
@@ -16,15 +16,47 @@ def read_cue_card(line):
         back += line[x]
         x += 1
     print("Back of cue card is: " + back)
-    next = input("When ready to see next cue card, press Enter")
+    next = input("When ready to see next cue card, press Enter, or to exit press 'X'")
+    return True if next == "X" or next == "x" else False;
+
+
+def first_menu_choice():
+    topic_name = input("Enter name (filename) of first set of cuecards: ")
+    topic = open(topic_name,'r') #opens the file of cue cards relating to topic to read
+    lines = topic.readlines()   #creates a list containing each line in the file
+    end = False
+    while len(lines) > 0 and not end:    #while there is still lines to be read
+        line = lines[random.randint(0,len(lines)-1)]  #randomly select a line
+        end = read_cue_card(line)
+        lines.remove(line)
+    print("Cue Card Reading has Finished")
     return
 
+def main_menu():
+    print("Select command from following list by entering the associated number/letter:")
+    print("     1. Read a set of cuecards")
+    print("     2. Add a set of cuecards")
+    print("     3. Edit a set of cuecards")
+    print("     4. Delete a set of cuecards")
+    print("     X. Exit program")
+    menu_choice = input()
+    if menu_choice == "1":
+        first_menu_choice()
+        main_menu()
+    elif menu_choice == "2":
+        print("Not a valid option at this time as program is still being developed. Please try again.")
+        main_menu()
+    elif menu_choice == "3":
+        print("Not a valid option at this time as program is still being developed. Please try again.")
+        main_menu()
+    elif menu_choice == "4":
+        print("Not a valid option at this time as program is still being developed. Please try again.")
+        main_menu()
+    elif menu_choice == "X" or menu_choice == "x":
+        print("Thank you for using this program")
+        return
+    else:
+        print("The option you entered was invalid, please try again:")
+        main_menu()
 
-topic_name = input("Enter name (filename) of first set of cuecards: ")
-topic = open(topic_name,'r') #opens the file of cue cards relating to topic to read
-lines = topic.readlines()   #creates a list containing each line in the file
-while len(lines) > 0:    #while there is still lines to be read
-    line = lines[random.randint(0,len(lines)-1)]  #randomly select a line
-    read_cue_card(line)
-    lines.remove(line)
-print("No more cue cards left on this topic")
+main_menu()
