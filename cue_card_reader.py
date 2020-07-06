acbb4280745c3e1531954802fbf5be055caf6136
Tasha <theblankrogue@gmail.com>
2020-07-06 10:45:55 +0100

    Added docstrings
    Added docstrings and commenting to program. Format is Google Comments and Docstrings Standard.

--- cue_card_reader.py
+++ cue_card_reader.py
@@ -2,6 +2,7 @@ import random
 import os
 
 def read_cue_card(line):
+    """Return formatted cuecard."""
     front = ""
     x = 0
     is_front = True
@@ -17,6 +18,15 @@ def read_cue_card(line):
 
 
 def read_cuecard_set(topics):
+    """Print cuecards in specified file.
+    
+    Goes through the lines in the file, the cue cards, printing
+    when prompted allowing the user to pass through the cue cards
+    at their own pace.
+
+    Args:
+        topics -- a list of all currently available sets of cue cards.
+    """
     topic_name = input("Enter name of first set of cuecards: ")
     topic_name = topic_name + ".txt"
     if topic_name not in topics:
@@ -42,12 +52,23 @@ def read_cuecard_set(topics):
 
 
 def add_cuecards(topics, edit_type):
+    """Add specified number of cue cards to a file.
+    
+    Adds a specified number of cue cards to an existing set if edit_type
+    is "a" or to a new set if edit_type is "w". The user is able to
+    input the specific cue cards in turn, then confirm that it's
+    correct.
+
+    Args:
+        topics -- a list of all currently available sets of cue cards.
+        edit_type -- a string that is "w" or "a".
+    """
     topic_name = input("Please enter the Topic Name: ")
     topic_name = topic_name + ".txt"
-    if topic_name not in topics and edit_type == "a":
+    if topic_name not in topics and edit_type == "a":   #if the topic does not already exist, and is expected to
         print("Invalid topic name.")
         return
-    elif topic_name not in topics and edit_type == "w":
+    elif topic_name not in topics and edit_type == "w":     #new topic is added to list of topics
         topics.append(topic_name)
     topic = open(topic_name, edit_type)
     numcards = int(input("How many cuecards do you want to add? "))
@@ -71,23 +92,33 @@ def add_cuecards(topics, edit_type):
     return
     
 def delete_set(topics):
+    """Delete a set of cue cards."""
     topic_name = input("Please enter the Topic to be deleted: ")
     topic_name = topic_name + ".txt"
     if topic_name not in topics:
         print("Invalid topic name.")
         return
-    os.remove(topic_name)
+    os.remove(topic_name)       #removes the file/set of cue cards from the folder
     topics.remove(topic_name)
     print("\n" + topic_name + " has been deleted.\n")
     return
 
 def print_topics(topics):
+    """Print list of topics."""
     print("Existing topics: \n")
     for i in range(len(topics)):
         print("     - " + topics[i] + "\n")
     return
 
 def main_menu(topics):
+    """Print menu for making selections on using the cue cards.
+    
+    This is the main/opening function that allows navigation
+    between the different functions of this program.
+
+    Args:
+        topics -- a list of all currently available sets of cue cards.
+    """
     print("Select command from following list by entering the associated number/letter:")
     print("     1. Read a set of cuecards")
     print("     2. Add a new set of cuecards")
@@ -120,6 +151,7 @@ def main_menu(topics):
 
 topics = os.listdir("C:\\Users\\ncoia\\Documents\\University\\cue_card_program")
 topics.remove("LICENSE")
-topics.remove(".git")
+topics.remove(".git")                       #removes files that are known not to be topics from the list of topics.
 topics.remove("cue_card_reader.py")
+topics.remove("README.md")
 main_menu(topics)
